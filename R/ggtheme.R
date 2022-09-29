1a3d26a9e2cdc7b0dbb4ae332e7f827d210c9ec4
Bernhard Bieri <74488547+BBieri@users.noreply.github.com>
2022-09-29 10:21:18 +0000

    Fixed some things in ggplot theme

--- R/ggtheme.R
+++ R/ggtheme.R
@@ -1,30 +1,32 @@
-#' Bernie's theme for ggplot 2 blog graphs.
+#' Bernie's theme for ggplot 2 blog graphs
 #'
 #' @return Theme for ggplot graphs in accordance with Bernie's blog colors.
 #' @export
 #'
 #' @examples
-#' library(ggplot2)
-#' # Prepare data
-#' cty_mpg <- aggregate(mpg$cty, by=list(mpg$manufacturer), FUN=mean)  # aggregate
-#' colnames(cty_mpg) <- c("make", "mileage")  # change column names
-#' cty_mpg <- cty_mpg[order(cty_mpg$mileage), ]  # sort
-#' cty_mpg$make <- factor(cty_mpg$make, levels = cty_mpg$make)
-#' # Plot
-#' ggplot(cty_mpg, aes(x=make, y=mileage)) +
-#'   geom_point(size=3) +
-#'   geom_segment(aes(x=make,
-#'                    xend=make,
-#'                    y=0,
-#'                    yend=mileage)) +
-#'   labs(title="Lollipop Chart",
-#'        subtitle="Make Vs Avg. Mileage",
-#'        caption="source: mpg") +
-#'   theme(axis.text.x = element_text(angle=65, vjust=0.6)) +
-#'   HohgantR::themebernie()
+#' \donttest{
+#'  library(ggplot2)
+#'  # Prepare data
+#'  cty_mpg <- aggregate(mpg$cty, by=list(mpg$manufacturer), FUN=mean)  # aggregate
+#'  colnames(cty_mpg) <- c("make", "mileage")  # change column names
+#'  cty_mpg <- cty_mpg[order(cty_mpg$mileage), ]  # sort
+#'  cty_mpg$make <- factor(cty_mpg$make, levels = cty_mpg$make)
+#'  # Plot
+#'  ggplot(cty_mpg, aes(x=make, y=mileage)) +
+#'    geom_segment(aes(x=make,
+#'                     xend=make,
+#'                     y=0,
+#'                     yend=mileage), color = "white") +
+#'    geom_point(size=3, color = "orange") +
+#'    labs(title="Lollipop Chart",
+#'         subtitle="Make Vs Avg. Mileage",
+#'         caption="source: mpg") +
+#'    HohgantR::themebernie() +
+#'    theme(axis.text.x = element_text(angle=65, vjust=0.6))
+#'  }
 
 themebernie <- function(){
   hrbrthemes::theme_modern_rc() +
-    ggplot2::theme(panel.background = element_rect(fill = "#222831"),
-                   plot.background = element_rect(fill = "#222831"))
+    ggplot2::theme(panel.background = ggplot2::element_rect(fill = "#222831"),
+                   plot.background = ggplot2::element_rect(fill = "#222831"))
 }
