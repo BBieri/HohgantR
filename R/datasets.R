00406eb4022d3f6b9e9682c1d109f20cbd5fe795
Bernhard Bieri <74488547+BBieri@users.noreply.github.com>
2023-01-13 22:03:58 +0100

    Added new example dataset with documentation

---
+++ R/datasets.R
@@ -0,0 +1,17 @@
+#' Synthetic example dataset of indicators
+#'
+#' Synthetic example dataset of indicators used to power a few examples in
+#' the package.
+#'
+#' @docType data
+#'
+#' @usage data(indicators_long)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(indicators_long)
+#' dplyr::glimpse(indicators_long)
+"indicators_long"
